d94dd18380adc7bd07ba6f468006006e00311c27
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-05-09 22:39:17 +0100

    - support ImageSharp

--- src/ImageDiff/Analyzers/CIE76Analyzer.cs
+++ src/ImageDiff/Analyzers/CIE76Analyzer.cs
@@ -15,13 +15,41 @@ namespace ImageDiff.Analyzers
 
     public bool[,] Analyze(Image<Rgba32> first, Image<Rgba32> second)
     {
+      var firstPx = new Rgba32[first.Width, first.Height];
+      first.ProcessPixelRows(acc =>
+      {
+        for (var y = 0; y < acc.Height; y++)
+        {
+          var pxRow = acc.GetRowSpan(y);
+          for (var x = 0; x < pxRow.Length - 1; x++)
+          {
+            ref var px = ref pxRow[x];
+            firstPx[x, y] = px;
+          }
+        }
+      });
+
+      var secondPx = new Rgba32[second.Width, second.Height];
+      second.ProcessPixelRows(acc =>
+      {
+        for (var y = 0; y < acc.Height; y++)
+        {
+          var pxRow = acc.GetRowSpan(y);
+          for (var x = 0; x < pxRow.Length - 1; x++)
+          {
+            ref var px = ref pxRow[x];
+            secondPx[x, y] = px;
+          }
+        }
+      });
+
       var diff = new bool[first.Width, first.Height];
       for (var x = 0; x < first.Width; x++)
       {
         for (var y = 0; y < first.Height; y++)
         {
-          var firstLab = CIELab.FromRGB(first.GetPixel(x, y));
-          var secondLab = CIELab.FromRGB(second.GetPixel(x, y));
+          var firstLab = CIELab.FromRGB(firstPx[x, y]);
+          var secondLab = CIELab.FromRGB(secondPx[x, y]);
 
           var score = Math.Sqrt(
             Math.Pow(secondLab.L - firstLab.L, 2) +
--- src/ImageDiff/Analyzers/ExactMatchAnalyzer.cs
+++ src/ImageDiff/Analyzers/ExactMatchAnalyzer.cs
@@ -7,13 +7,40 @@ namespace ImageDiff.Analyzers
   {
     public bool[,] Analyze(Image<Rgba32> first, Image<Rgba32> second)
     {
+      var firstPx = new Rgba32[first.Width, first.Height];
+      first.ProcessPixelRows(acc =>
+      {
+        for (var y = 0; y < acc.Height; y++)
+        {
+          var pxRow = acc.GetRowSpan(y);
+          for (var x = 0; x < pxRow.Length - 1; x++)
+          {
+            ref var px = ref pxRow[x];
+            firstPx[x, y] = px;
+          }
+        }
+      });
+
+      var secondPx = new Rgba32[second.Width, second.Height];
+      second.ProcessPixelRows(acc =>
+      {
+        for (var y = 0; y < acc.Height; y++)
+        {
+          var pxRow = acc.GetRowSpan(y);
+          for (var x = 0; x < pxRow.Length - 1; x++)
+          {
+            ref var px = ref pxRow[x];
+            secondPx[x, y] = px;
+          }
+        }
+      });
       var diff = new bool[first.Width, first.Height];
       for (var x = 0; x < first.Width; x++)
       {
         for (var y = 0; y < first.Height; y++)
         {
-          var firstPixel = first.GetPixel(x, y);
-          var secondPixel = second.GetPixel(x, y);
+          var firstPixel = firstPx[x, y];
+          var secondPixel = secondPx[x, y];
           if (firstPixel != secondPixel)
           {
             diff[x, y] = true;
